0669c08d2369964814b2c4ec3b74bc05de0e7747
Justin Mitchel <hello@teamcfe.com>
2018-12-15 15:33:58 -0800

    3 - Access Keys and Config

--- src/main.py
+++ src/main.py
@@ -2,19 +2,36 @@ import boto3
 
 # import from elsewhere
 # Django "settings" module
-AWS_ACCESS_KEY_ID = None
-AWS_SECRET_ACCESS_KEY = None
-AWS_S3_REGION_NAME = None
-AWS_STORAGE_BUCKET_NAME = None
+
+AWS_ACCESS_KEY_ID = 'AKIAJMLS2HKIPMBXJMMA' 
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_STORAGE_BUCKET_NAME = 'aws-cfe-intro'
 
 class AWS:
-    access_key = AWS_ACCESS_KEY_ID
+    access_key      = AWS_ACCESS_KEY_ID
+    secret_key      = AWS_SECRET_ACCESS_KEY
+    region          = AWS_S3_REGION_NAME
+    bucket          = AWS_STORAGE_BUCKET_NAME
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def get_s3_client(self):
-        return boto.client('s3')
+        s3_client = boto.client('s3'
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key,
+                region_name = self.region
+            )
+        return s3_client
 
+    def get_download_url(self):
+        return 
+
+    def presign_post_url(self):
+        return 
+
+
+# AWS().get_download_url()
 
